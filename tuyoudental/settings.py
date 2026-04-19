eb67dd4a404871624326a2dc99562ed9f190c042
notsuz <ifosuz77@gmail.com>
2026-04-19 22:39:40 +0545

    cloud setup env managed

--- tuyoudental/settings.py
+++ tuyoudental/settings.py
@@ -14,10 +14,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-@hv&r&35v_1b$-5@2-r+c%$jd3^(yx_&cefq&7gw9zta%05*vm')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG')
+DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
 ALLOWED_HOSTS = ["*"]
 
